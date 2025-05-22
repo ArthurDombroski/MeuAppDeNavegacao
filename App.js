abfde605d8c9f96e029fe2d41b7d42405f8ecbc7
Arthur Dombroski <tutudombroski@gmail.com>
2025-05-22 17:13:43 -0300

    Conserto Bug

--- App.js
+++ App.js
@@ -14,8 +14,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+        
         <Stack.Screen name="Profile" component={ProfileScreen} options ={{headerShown :false}} />
         <Stack.Screen name="Details" component={DetailsScreen} options ={{headerShown :false}} />
       </Stack.Navigator>
